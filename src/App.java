5004ba94daf3d0a024cc8f347e1ed581cbf8b66c
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-11 14:43:29 -0300

    User getters and setters

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        User user = new User();
+
+        System.out.println(user.getNome());
+        System.out.println(user.getEmail());
+        System.out.println(user.getSenha());
+        System.out.println(user.getIdade());
+        System.out.println(user.getAulasAssistidas());
+
+        user.setNome("Audrey Costa");
+        user.setEmail("audrey@gmail.com");
+        user.setSenha("banana");
+        user.setIdade(26);
+        user.assistirAula();
+        user.assistirAula();
+        user.assistirAula();
+        user.assistirAula();
+
+        System.out.println(user.getNome());
+        System.out.println(user.getEmail());
+        System.out.println(user.getSenha());
+        System.out.println(user.getIdade());
+        System.out.println(user.getAulasAssistidas());
+
+        user.setNovaSenha(user.getSenha(), "batata");
+        user.setNovoEmail("audrey@yahoo.com", "banana");
+        user.assistirAula();
+        user.assistirAula();
+
+        System.out.println(user.getNome());
+        System.out.println(user.getEmail());
+        System.out.println(user.getSenha());
+        System.out.println(user.getIdade());
+        System.out.println(user.getAulasAssistidas());
+
+        System.out.println(user.responder());
     }
 }
--- src/User.java
+++ src/User.java
@@ -18,4 +18,60 @@ public class User {
     String resposta = reader.nextLine();
     return resposta;
   }
+
+  public String getNome(){
+    return this.nome;
+  }
+  
+  public void setNome(String nome) {
+    if (this.nome == null) {
+      this.nome = nome;
+    }
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    if (this.email == null){
+      this.email = email;
+    }
+  }
+  
+  public void setNovoEmail(String email, String senha) {
+    if (this.senha == senha){
+      this.email = email;
+    }
+  }
+
+  public String getSenha(){
+    return this.senha;
+  }
+
+  public void setSenha(String senha) {
+    if (this.senha == null){
+      this.senha = senha;
+    }
+  }
+
+  public void setNovaSenha(String senha, String novaSenha){
+    if (this.senha == senha){
+      this.senha = novaSenha;
+    }
+  }
+
+  public int getIdade(){
+    return this.idade;
+  }
+
+  public void setIdade(int idade) {
+    if (this.idade == 0) {
+      this.idade = idade;
+    }
+  }
+
+  public int getAulasAssistidas() {
+    return aulasAssistidas;
+  }
 }
